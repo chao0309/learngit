3e1a8d2dc269a079a561c0a8b5a84ca8f3130a7f
wang chao <2427671947@qq.com>
2018-05-17 14:41:36 +0800

    first commit

---
+++ TestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            string b = "5";
+            string c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ztjs/ztjs.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using SufeiUtil;
+using Newtonsoft.Json;
+
+namespace ztjs
+{
+    /// <summary>
+    /// ztjs 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
+    // [System.Web.Script.Services.ScriptService]
+    public class ztjs : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod]
+        public string getProject()
+        {
+            string strSql = "select DeptName  from  T_E_Org_Department where TypeID='FGS'";
+            try
+            {
+                DataTableCollection tablecollection = SqlHelper.GetTableText(strSql,null);
+                DataTable table = tablecollection[0];
+                string json = JsonConvert.SerializeObject(table);
+                return json;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
